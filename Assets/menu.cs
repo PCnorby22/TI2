a8174baf8c4a808946f7e4ddf363f847e9dedc07
PCnorby22 <bigleite10@gmail.com>
2025-03-27 10:31:22 -0300

    gustavo
    new input e scenes

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,7 +13,23 @@ public class menu : MonoBehaviour
     // Start is called before the first frame update
     public void play()
     {
-        SceneManager.LoadScene("jogo");
+        SceneManager.LoadScene("menu fase");
+    }
+    public void Fase1()
+    {
+        SceneManager.LoadScene("fase1");
+    }
+    public void Fase2()
+    {
+        SceneManager.LoadScene("menu fase");
+    }
+    public void Fase3()
+    {
+        SceneManager.LoadScene("menu fase");
+    }
+    public void loja()
+    {
+        SceneManager.LoadScene("menu loja");
     }
     public void config()
     {
@@ -21,7 +38,18 @@ public class menu : MonoBehaviour
     }
     public void voltar()
     {
-        inicial.SetActive(true);
-        configuraçao.SetActive(false);
+        if (SceneManager.GetActiveScene().name == "inicio")
+        {
+            inicial.SetActive(true);
+            configuraçao.SetActive(false);
+        }
+        else
+        {
+            voltarmenu();
+        }
+    }
+    public void voltarmenu()
+    {
+        SceneManager.LoadScene("inicio");
     }
 }
