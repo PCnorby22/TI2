ee5e47e0d8e1d20b93614501f32b87936728cfeb
gammawizard12345 <162822629+gammawizard12345@users.noreply.github.com>
2025-04-16 19:59:17 -0300

    Adicionei o comeco de funcionalidade de inimigo

---
+++ Assets/scripts/moveEnemy.cs
@@ -0,0 +1,76 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.UI;
+
+public class moveEnemy : MonoBehaviour
+{
+    public GameObject voltar, reiniciar, derrota, vitoria;
+    public Slider distancia;
+    Rigidbody rb;
+    public int velocidade, vida;
+    public Object Enemy;
+    
+    public TextMeshProUGUI vidaTela;
+    Vector3 inicio;
+    private void Awake()
+    {
+        
+  
+    }
+    private void OnEnable()
+    {
+       
+    }
+    private void OnDisable()
+    {
+    }
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        inicio = this.transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
+    }
+  
+
+
+    private void EnemyMovement()
+    {
+        if (this.transform.position.x < -15)
+        {
+            this.transform.position = new Vector3(-15, this.transform.position.y, this.transform.position.z);
+        }
+        else if (this.transform.position.x > 15)
+        {
+            this.transform.position = new Vector3(15, this.transform.position.y, this.transform.position.z);
+        }
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, 1 * velocidade * Time.fixedDeltaTime);
+        Vector3 dis = this.transform.position - inicio;
+        distancia.value = ((int)dis.magnitude);
+        if(Time.deltaTime%20 == 0)
+        {
+            velocidade = velocidade/2;
+        }
+        if (Time.deltaTime%30 == 0)
+        {
+            velocidade = velocidade*2;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "player")
+        {
+            vida--;
+           
+        }
+    }
+}
