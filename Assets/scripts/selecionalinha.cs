34f94c66b5660c52096e69a65c53b2bad0df9e1f
PCnorby22 <bigleite10@gmail.com>
2025-04-24 14:18:26 -0300

    gustavo

---
+++ Assets/scripts/selecionalinha.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class selecionalinha : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
